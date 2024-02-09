5e0b24ddf7ed7154e98bd536a4d87e077e3f0367
niels demeyer <niels.demeyer99@gmail.com>
2024-02-09 10:09:21 +0100

    function management

--- main.py
+++ main.py
@@ -2,33 +2,28 @@ import json
 from utils.openspace import Openspace
 from utils.file_utils import load_colleagues
 from utils.file_utils import load_json
+from utils.file_utils import compare_json_with_new_colleagues
 
 
-def compare_json_with_new_colleagues(config):
-    config = load_json('config.json')
-    colleagues = load_colleagues('new_colleagues.txt')
-    new_colleagues = [colleague for colleague in colleagues if colleague not in config['colleagues_to_remove']]
-    new_colleagues += [colleague for colleague in config['colleagues'] if colleague not in new_colleagues]
-    return new_colleagues
-        
-
-config = load_json('config.json')
+config = load_json("config.json")
 # Load the colleagues from the excel file
 colleagues = compare_json_with_new_colleagues(config=config)
 
 # Create an openspace and organize the colleagues
-openspace = Openspace(number_of_tables=config['number_of_tables'], table_capacity=config['table_capacity'])
+openspace = Openspace(
+    number_of_tables=config["number_of_tables"], table_capacity=config["table_capacity"]
+)
 # Check the capacity
 openspace.check_capacity()
 # Organize the colleagues
 openspace.organize(colleagues)
 # Display the results
-openspace.display() 
+openspace.display()
 
 
 # Print the total number of seats, the total number of people, and the number of remaining seats
 print(f"Total seats: {openspace.total_seats()}")
 print(f"Total people: {openspace.total_people()}")
 print(f"Remaining seats: {openspace.remaining_seats()}")
-print(f"config: {config}") 
+print(f"config: {config}")
 print(f"colleagues: {colleagues}")
--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 
+
 def load_colleagues(filepath):
     """
     This function reads a text file where each line represents a colleague.
@@ -13,12 +14,13 @@ def load_colleagues(filepath):
     Returns:
         list: A list of lists, where each inner list represents a colleague.
     """
-    with open(filepath, 'r') as file:
+    with open(filepath, "r") as file:
         lines = file.readlines()
     colleagues = []
     for line in lines:
-        colleagues.append(line.strip().split(','))
+        colleagues.append(line.strip().split(","))
     return colleagues
+
 
 def load_json(filepath):
     """
@@ -30,6 +32,22 @@ def load_json(filepath):
     Returns:
         dict: A dictionary with the data from the JSON file.
     """
-    with open(filepath, 'r') as file:
+    with open(filepath, "r") as file:
         data = json.load(file)
     return data
+
+
+def compare_json_with_new_colleagues(config):
+    config = load_json("config.json")
+    colleagues = load_colleagues("new_colleagues.txt")
+    new_colleagues = [
+        colleague
+        for colleague in colleagues
+        if colleague not in config["colleagues_to_remove"]
+    ]
+    new_colleagues += [
+        colleague
+        for colleague in config["colleagues"]
+        if colleague not in new_colleagues
+    ]
+    return new_colleagues
